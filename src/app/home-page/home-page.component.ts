45531ebfaa5cf4632866b7acdddb38ef8b33eb7b
acubick <acubick@gmail.com>
2020-04-03 14:37:04 +0300

    сделан вывод поста на главной странице
    Took 14 minutes

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Post } from '../shared/interfaces';
+import { PostsService } from '../shared/posts.service';
 
 @Component({
   selector: 'app-home-page',
@@ -6,10 +9,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home-page.component.scss']
 })
 export class HomePageComponent implements OnInit {
-
-  constructor() { }
+   post$: Observable<Post[]>
+  constructor(
+    private postsService: PostsService
+  ) { }
 
   ngOnInit() {
+   this.post$ = this.postsService.getAll()
   }
 
 }
